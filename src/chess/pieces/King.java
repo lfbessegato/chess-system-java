df414a79b1d25bb6dec37b0e4739b0d5be86cbfc
Luciano Bessegato <lfrbessegato@gmail.com>
2019-08-16 14:39:22 -0300

    Implementing possible moves of king

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -8,6 +9,11 @@ public class King extends ChessPiece {
 
 	public King(Board board, Color color) {
 		super(board, color);
+	}
+	
+	private boolean canMove(Position position) {
+		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		return p == null || p.getColor() != getColor();
 	}
 	
 	@Override
@@ -20,6 +26,56 @@ public class King extends ChessPiece {
 		// TODO Auto-generated method stub
 		
 		boolean [][] mat = new boolean [getBoard().getRows()][getBoard().getColumns()];
+		
+		Position p = new Position(0, 0);
+		
+		//Above - Acima
+		p.setValues(position.getRow()-1, position.getColumn());
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//below - Abaixo
+		p.setValues(position.getRow()+1, position.getColumn());
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//Left - Esquerda
+		p.setValues(position.getRow(), position.getColumn()-1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//right - Direita
+		p.setValues(position.getRow(), position.getColumn()+1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//nw - noroeste / diagonal
+		p.setValues(position.getRow() -1, position.getColumn()-1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//ne - nordeste / diagonal
+		p.setValues(position.getRow() -1, position.getColumn()+1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//sw - sudoeste / diagonal
+		p.setValues(position.getRow() +1, position.getColumn()-1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//se - sudeste / diagonal
+		p.setValues(position.getRow() +1, position.getColumn()+1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
 		return mat;
 	}
 
